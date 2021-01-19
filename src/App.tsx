51cbcfabb79eb9de9ecee0eefa060652bd1b17c2
Yohann Legrand <yoh.legrand@gmail.com>
2021-01-19 14:40:54 +0100

    feat(app): app basic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Button, Container, Spinner } from 'react-bootstrap'
+import TacticBoard from './components/TacticBoard';
+import { fetchTactic } from './services/fetchTactic';
+import Tactic from './types/Tactic';
+import { getSideToPlayFromFen } from './utils/TacticBoardUtils';
 
 function App() {
+  const [tactics, setTactics] = useState<Tactic[]>([])
+  const [hint, setHint] = useState<"sideToPlay" | "incorrect" | "correct" | "solved">("sideToPlay")
+
+  const loadTactic = async () => {
+    try {
+      const newTactic = await fetchTactic()
+      setTactics((it) => it.concat(newTactic))
+    } catch (error) {
+      console.error("Error loading tactic", { error })
+    }
+  }
+
+  useEffect(() => {
+    loadTactic()
+    loadTactic()
+  }, [])
+
+  if (tactics.length === 0) {
+    // TODO
+    return (
+      <Container>
+        <Spinner role="status" animation="border" />
+        Loading...
+      </Container>
+    )
+  }
+
+  const tactic = tactics[0]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className="text-center">
+      <h1>Tactics Trainer</h1>
+      <div className="d-flex justify-content-center">
+        <TacticBoard
+          key={tactic.id}
+          tactic={tactic}
+          onCorrect={() => {
+            setHint("correct")
+            setTimeout(() => setHint("sideToPlay"), 1000)
+          }}
+          onIncorrect={() => {
+            setHint("incorrect")
+            setTimeout(() => setHint("sideToPlay"), 1000)
+          }}
+          onSolve={() => {
+            setHint("solved")
+            loadTactic()
+            setTimeout(() => {
+              setTactics((it) => it.slice(1))
+              setHint("sideToPlay")
+            }, 1000)
+          }}
+        />
+      </div>
+      <div className="text-center mt-3">
+        {hint === "sideToPlay" && (
+          <Button variant="dark">
+            {getSideToPlayFromFen(tactic.fen) === "b" ? "White" : "Black"} to move
+          </Button>
+        )}
+        {hint === "correct" && (
+          <Button variant="primary">
+            Correct!
+          </Button>
+        )}
+        {hint === "incorrect" && (
+          <Button variant="danger">
+            Incorrect!
+          </Button>
+        )}
+        {hint === "solved" && (
+          <Button variant="success">
+            Solved!
+          </Button>
+        )}
+      </div>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/services/fetchTactic.ts
@@ -0,0 +1,16 @@
+import axios from "axios"
+import Tactic from "../types/Tactic"
+
+export const fetchTactic = async (): Promise<Tactic> => {
+  const res = await axios.post(
+    `${process.env.REACT_APP_CHESSBLUNDERS_API}/blunder/get`,
+    { type: 'explore' }
+  )
+  const data = res.data.data
+  return {
+    id: data.id,
+    fen: data.fenBefore,
+    blunderMove: data.blunderMove,
+    solution: data.forcedLine,
+  }
+}
