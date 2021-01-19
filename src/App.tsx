0ac8b0b507ca9b108405015943c071d27675fe31
Yohann Legrand <yoh.legrand@gmail.com>
2021-01-19 21:19:01 +0100

    feat(game): next and restart buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Spinner } from 'react-bootstrap'
+import { Container, Spinner, Button } from 'react-bootstrap'
+import { ArrowRight, RefreshCw } from 'react-feather'
+import styled from 'styled-components/macro'
+
 import TacticBoard from './components/TacticBoard'
 import { fetchTactic } from './services/fetchTactic'
 import Tactic from './types/Tactic'
 import { getSideToPlayFromFen } from './utils/TacticBoardUtils'
 import GameStatus from './components/GameStatus'
 
+const ControlStrip = styled.div`
+  width: 400px;
+  margin: 30px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
 const App: React.FC = () => {
   const [tactics, setTactics] = useState<Tactic[]>([])
+  const [boardKey, setBoardKey] = useState<number>(Date.now())
   const [hint, setHint] = useState<'sideToPlay' | 'incorrect' | 'correct' | 'solved'>('sideToPlay')
 
   // Fetch a new tactic from API
@@ -39,12 +51,13 @@ const App: React.FC = () => {
 
   // Render
   const tactic = tactics[0]
+  console.log(tactic.solution)
   return (
     <Container className="text-center">
       <h1>Tactics Trainer</h1>
       <div className="d-flex justify-content-center">
         <TacticBoard
-          key={tactic.id}
+          key={`${tactic.id}-${boardKey}`}
           tactic={tactic}
           onCorrect={() => {
             setHint('correct')
@@ -64,9 +77,28 @@ const App: React.FC = () => {
           }}
         />
       </div>
-      <div className="text-center mt-3">
+      <ControlStrip>
+        <Button
+          variant="light"
+          onClick={() => {
+            setBoardKey(Date.now())
+            setHint('sideToPlay')
+          }}
+        >
+          <RefreshCw className="mr-1" /> Restart
+        </Button>
         <GameStatus hint={hint} whiteTurn={getSideToPlayFromFen(tactic.fen) === 'b'} />
-      </div>
+        <Button
+          variant="light"
+          onClick={() => {
+            loadTactic()
+            setTactics((it) => it.slice(1))
+            setHint('sideToPlay')
+          }}
+        >
+          Next <ArrowRight className="ml-1" />
+        </Button>
+      </ControlStrip>
     </Container>
   )
 }
